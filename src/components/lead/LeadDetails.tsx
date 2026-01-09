19a5dd519733b360231efa287be017c1e6da6139
Gonzalo <gonzamaunas@gmail.com>
2026-01-09 02:46:21 -0300

    Agregada función para castear el field a su tipo correspondiente.

--- src/components/lead/LeadDetails.tsx
+++ src/components/lead/LeadDetails.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { useParams } from "react-router-dom"
 import { getLead } from "./leadService.ts"
 import { Box, Chip, Container, Divider, Grid, Paper, Typography } from "@mui/material"
 import type { Lead } from "../../types/leads.ts"
+import { getFieldType } from "../../generalService.ts"
 
 export const LeadDetails = () => {
 
@@ -20,45 +21,65 @@ export const LeadDetails = () => {
     console.log(lead)
     return (
         <>
-        <Container maxWidth={false}>
-            <Grid container spacing={3}>
-                <Grid size={3} >
-                    <Paper sx={{ minHeight: "100%", p: 2, borderRadius:"1em" }}>
-                        
-                        <Box sx={{ display: "flex", justifyContent:"end", alignItems:"center", flexWrap:"wrap" }}>
-                        <Typography variant="h2" sx={{flexGrow: 1, minWidth: "fit"}}>{lead?.field_values[0].value}</Typography>
-                            <Chip label={lead?.active ? "Habilitado" : "Deshabilitado"} color={lead?.active ? "success" : "error"} sx={{justifySelf:"end"}}/>
-                        </Box>
+            <Container maxWidth={false}>
+                <Grid container spacing={3}>
+                    <Grid size={3} >
+                        <Paper sx={{ minHeight: "100%", p: 2, borderRadius: "1em" }}>
 
-                        <Divider sx={{ my: 2 }} />
-                        <Typography variant="h3">Información General</Typography>
+                            {lead &&
+                                <>
+                                    <Box sx={{ display: "flex", justifyContent: "end", alignItems: "center", flexWrap: "wrap" }}>
+                                        <Typography variant="h2" sx={{ flexGrow: 1, minWidth: "fit" }}>{lead?.field_values[0].value} {lead?.field_values[1].value}</Typography>
+                                        <Chip label={lead?.active ? "Habilitado" : "Deshabilitado"} color={lead?.active ? "success" : "error"} sx={{ justifySelf: "end" }} />
+                                    </Box>
 
-                        {lead?.field_values.map((item, idx) => {
-                            if (idx > 0) {
-                                return (
-                                    <div key={`${idx}`} >
-                                        <Typography sx={{ fontWeight: "bold" }}>{item.field.name}</Typography>
-                                        <Typography>{item.value}</Typography>
-                                    </div>
-                                )
+                                    <Divider sx={{ my: 2 }} />
+                                    <Typography variant="h3">Información General</Typography>
 
-                            }
-                        })}
-                        <Divider sx={{ my: 2 }} />
-                        <Typography sx={{ fontWeight: "bold" }}>Fecha de Creación</Typography>
-                        <Typography>{lead?.created_at}</Typography>
-                        <Typography sx={{ fontWeight: "bold" }}>Fecha de Última Modificación</Typography>
-                        <Typography>{lead?.updated_at}</Typography>
-                    </Paper>
+                                    {lead?.field_values.map((item, idx) => {
+                                        if (idx > 1) {
+                                            return (
+                                                <LeadField key={`${idx}`} fieldName={item.field.name} value={item.value} type={item.field.field_type_code} />
+                                            )
+                                        }
+                                    })}
+                                    <Divider sx={{ my: 2 }} />
+                                    <LeadField fieldName="Fecha de Creación" value={lead.created_at} type="DATE" />
+                                    <LeadField fieldName="Fecha de Última Modificación" value={lead.updated_at} type="DATE" />
+
+                                </>}
+                        </Paper>
+                    </Grid>
+                    <Grid size={9}>
+                        <Paper sx={{ minHeight: "100%", p: 2, borderRadius: "1em" }}>
+                            <Typography variant="h2">Actividades</Typography>
+                        </Paper>
+                    </Grid>
                 </Grid>
-                <Grid size={9}>
-                    <Paper sx={{ minHeight: "100%", p: 2, borderRadius:"1em" }}>
-                        <Typography variant="h2">Actividades</Typography>
-                    </Paper>
-                </Grid>
-            </Grid>
 
-        </Container>
+            </Container>
+        </>
+    )
+}
+
+interface LeadFieldProps {
+    fieldName: string,
+    value: string | number | boolean,
+    type: string
+}
+
+export const LeadField = ({ fieldName, value, type }: LeadFieldProps) => {
+
+    const fieldValue = useMemo(() => getFieldType(type, value), [type, value])
+
+    if (type === "BOOL" && fieldValue) {
+        return <Chip color="success" label={fieldName} sx={{ marginBlock: ".5rem", fontWeight: "bold" }} />
+    }
+
+    return (
+        <>
+            <Typography sx={{ fontWeight: "bold" }}>{fieldName}:</Typography>
+            <Typography sx={{ paddingLeft: ".5rem" }}>{value}</Typography>
         </>
     )
 }
--- src/generalService.ts
+++ src/generalService.ts
@@ -1,1 +1,15 @@
 export const API_BASE_URL = 'http://localhost:8000';
+
+export const getFieldType = (fieldType : string, 
+    value: string | boolean | number) => {
+        if (typeof value !== "string") return value
+        
+    switch (fieldType){
+        case "INT":
+        case "NUMBER":
+            return parseInt(value)
+        case "BOOL":
+            return Boolean(value)
+    }
+    return value
+}
